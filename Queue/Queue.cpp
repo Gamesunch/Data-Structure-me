cc1a9e67770574dee1daee9aa0f894f19adcddd5
Chatos_Xeno <105534510+Gamesunch@users.noreply.github.com>
2024-07-31 15:02:18 +0700

    Add files via upload

---
+++ Queue/Queue.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Queue{
+	public:
+		int Array[4];
+		int size = 4;
+		int head = 0;
+		int tail = -1;
+		
+		bool full(){
+			return s() == size;
+		}
+		
+		bool empty(){
+			return s() == 0;
+		}
+		
+		void enqueue(int data)
+		{
+			if(!full()){
+				tail++;
+				Array[tail%size] = data;
+			}
+			else{
+				cout<<"FULL!!!"<<endl;
+			}
+			
+		}
+		
+		int dequeue()
+		{
+			if(!empty()){
+				int temp = head;
+				head++;
+				return Array[temp%size];
+			}
+			else{
+				cout<<"EMPTY!!!"<<endl;
+			}
+			
+			
+		}
+		
+		void print()
+		{
+			int n = 0;
+			for(int i = head;i <= tail;i++)
+			{
+				cout<<Array[i%size]<<" ";
+			}
+			cout<<endl;
+		}
+		
+		int s()
+		{
+			int n = 0;
+			for(int i = head;i <= tail;i++)
+			{
+				n++;
+			}
+			return n;
+		}
+		
+		void showData()
+		{
+			cout<<Array[head%size]<< " " << Array[tail%size] << endl;
+		}
+};
+
+int main(){
+  string input;
+  Queue q;
+  
+  while(true)
+  {
+  	cin >> input;
+  	if(input == "e")
+	  {
+  		int num;
+  		cin >> num;
+  		q.enqueue(num);
+		
+	  }
+	  
+	if(input == "d")
+	{
+		if(q.s() != 0){
+			cout<<q.dequeue()<<endl;
+		}
+		else{
+			cout<<"EMPTY!!!"<<endl;
+		}
+	}
+	
+	if(input == "p")
+	{
+		q.print();
+	}
+	
+	if(input == "n")
+	{
+		cout<<q.s()<<endl;
+	}
+	
+	if(input == "s")
+	{
+		q.showData();
+	}
+	
+	if(input == "x")
+	{
+		break;
+	}
+  }
+  
+  return 0;
+}
