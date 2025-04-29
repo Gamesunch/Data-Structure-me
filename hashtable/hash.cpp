2ae00e958d0bdb8c865321065a825cc0d31167ae
Chatos_Xeno <105534510+Gamesunch@users.noreply.github.com>
2025-04-29 13:49:26 +0700

    Update hash.cpp

--- hashtable/hash.cpp
+++ hashtable/hash.cpp
@@ -5,110 +5,125 @@
 using namespace std;
 
 class Node {
-  public:
+public:
     int key;
     string data;
     Node* next;
 
     Node() {
-      key = -1;
-      data = "-";
-      next = NULL;
+        key = -1;
+        data = "-";
+        next = NULL;
     }
 
     Node(int k, string d) {
-      key = k;
-      data = d;
-      next = NULL;
+        key = k;
+        data = d;
+        next = NULL;
     }
 };
 
 class HashTable {
-  public:
+public:
     Node* table[SIZE];
 
     HashTable() {
-      for (int i = 0; i < SIZE; i++) {
-        table[i] = new Node();
-      }
+        for (int i = 0; i < SIZE; i++) {
+            table[i] = new Node();
+        }
     }
-  
-    void add(int key, string data) {
-      int i = key % SIZE;
-      //find exit key
-      for (int i = 0; i < SIZE; i++) {
-        if(table[i]->key == key){
-          cout<<"Key already exists"<<endl;
-          return;
-        }
-        Node* s = table[i]->next;
-        while (s != NULL) {
-          if(s->key == key){
-            cout<<"Key already exists"<<endl;
-            return;
-          }
-          s = s->next;
-        }
-      }
 
-      if (table[i]->key == -1) {
-        table[i]->key = key;
-        table[i]->data = data;
-      } else {
-        Node* n = table[i];
-        while (n->next != NULL) {
-          n = n->next;
+    ~HashTable() {
+        for (int i = 0; i < SIZE; ++i) {
+            Node* curr = table[i];
+            while (curr != nullptr) {
+                Node* temp = curr;
+                curr = curr->next;
+                delete temp;
+            }
         }
-        n->next = new Node(key, data);
-      }
+    }
+
+    void add(int key, string data) {
+        int index = key % SIZE;
+
+        // Check if the key already exists
+        for (int j = 0; j < SIZE; j++) {
+            if (table[j]->key == key) {
+                cout << "Key already exists" << endl;
+                return;
+            }
+            Node* s = table[j]->next;
+            while (s != nullptr) {
+                if (s->key == key) {
+                    cout << "Key already exists" << endl;
+                    return;
+                }
+                s = s->next;
+            }
+        }
+
+        // Add new node
+        if (table[index]->key == -1) {
+            table[index]->key = key;
+            table[index]->data = data;
+        } else {
+            Node* n = table[index];
+            while (n->next != nullptr) {
+                n = n->next;
+            }
+            n->next = new Node(key, data);
+        }
     }
 
     string search(int key) {
-      int i = key % SIZE;
-      Node* n = table[i];
-      while (n != NULL) {
-        if (n->key == key) {
-          return n->data;
+        int index = key % SIZE;
+        Node* n = table[index];
+        while (n != nullptr) {
+            if (n->key == key) {
+                return n->data;
+            }
+            n = n->next;
         }
-        n = n->next;
-      }
-      return "-";
+        return "-";
     }
 
     void print() {
-      for (int i = 0; i < SIZE; i++) {
-        cout << "(" << table[i]->key << "," << table[i]->data << ")";
-        Node* n = table[i]->next;
-        while (n != NULL) {
-          cout << " (" << n->key << "," << n->data << ")";
-          n = n->next;
+        for (int i = 0; i < SIZE; i++) {
+            cout << "(" << table[i]->key << "," << table[i]->data << ")";
+            Node* n = table[i]->next;
+            while (n != nullptr) {
+                cout << " (" << n->key << "," << n->data << ")";
+                n = n->next;
+            }
+            cout << endl;
         }
-        cout << endl;
-      }
     }
 };
 
 int main() {
-  HashTable h;
-  
-  string command;
-  int key;
-  string data;
+    HashTable h;
 
-  while(true){
-    cin>>command;
-    if(command == "e"){
-      break;
+    string command;
+    int key;
+    string data;
+
+    while (true) {
+        cin >> command;
+        if (command == "e") {
+            break;
+        }
+
+        if (command == "a") {
+            cin >> key >> data;
+            h.add(key, data);
+        } else if (command == "s") {
+            cin >> key;
+            cout << h.search(key) << endl;
+        } else if (command == "p") {
+            h.print();
+        }
     }
 
-    if(command == "a"){
-      cin>>key>>data;
-      h.add(key, data);
-    } else if(command == "s"){
-      cin>>key;
-      cout<<h.search(key)<<endl;
-    } else if(command == "p"){
-      h.print();
-    }
-  }
+    return 0;
 }
